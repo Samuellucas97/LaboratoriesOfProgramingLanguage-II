afb611b53c2548bdee5a9413b34a3a5d0e936c22
Samuel Lucas <samuel1797@gmail.com>
2018-05-24 09:47:32 -0300

    Update Calculadora.java

--- PraticalClassEighteen/QuestionOne/src/Calculadora.java
+++ PraticalClassEighteen/QuestionOne/src/Calculadora.java
@@ -1,43 +1,41 @@
-/**
- * @file      Calculadora.java
- * @brief     Contém a classe Calculadora
- * @author    Samuel Lucas de Moura Ferino
- * @since     24.04.2018
- * @version   0.0.3
- */ 
- 
+
+// -> PACOTES IMPORTADOS
+
 import java.util.Stack;
 import java.util.Scanner;
 
 /**
- * @class     Calculadora
+ * Implementação das operações da interface Calculavel
+ * @see       Calculavel
+ * @author    Samuel Lucas de Moura Ferino
+ * @since     24.04.2018
+ * @version   0.0.3
  */ 
 public class Calculadora implements Calculavel{
  
-  /// ATRIBUTOS
+  // -> ATRIBUTOS
   
-  /**<  Pilha que contém o histórico */
-  Stack<Double> historico;
+  Stack<Double> historico;  // ->  Pilha que contém o histórico 
  
-  /// MÉTODOS
+  // -> MÉTODOS
   
   /**
-   * @brief   Construtor padrão
+   * Construtor padrão
    */ 
   public Calculadora(){
     historico = new Stack<Double>();
   } 
   
   /**
-   * @brief   Liga a calculadora e espera os operandos e o operador  
+   * Liga a calculadora e espera os operandos e o operador  
    */ 
   public void operacao(){
 
     System.out.print("Informe o primeiro valor:");
     Scanner ler = new Scanner(System.in);
-    historico.push(ler.nextDouble() );    /// A
+    historico.push(ler.nextDouble() );    // -> PRIMEIRO OPERANDO 
     System.out.print("\nInforme o segundo valor:");
-    historico.push(ler.nextDouble() );    /// B
+    historico.push(ler.nextDouble() );    // -> SEGUNDO OPERANDO
     
     double operador = -1;
     System.out.print("\nInforme o caractere que simboliza a operacao:");
@@ -53,7 +51,7 @@ public class Calculadora implements Calculavel{
         break;
     }
     
-    historico.push(operador);       /// OPERACAO
+    historico.push(operador);       // -> OPERACAO
     
     double operacao = historico.pop();
     double b = historico.pop();
@@ -61,16 +59,16 @@ public class Calculadora implements Calculavel{
       
     double resultado;  
       
-    if( operacao == 0){      /// SOMA
+    if( operacao == 0){      // -> SOMA
       resultado = soma(a,b);
     }
-    else if( operacao == 1){  /// SUBTRAÇÃO
+    else if( operacao == 1){  // -> SUBTRAÇÃO
       resultado = subtracao(a,b);
     }
-    else if( operacao == 2){  /// MULTIPLICAÇÃO
+    else if( operacao == 2){  // -> MULTIPLICAÇÃO
       resultado = multiplicacao(a,b);
     }
-    else{                     /// DIVISÃO
+    else{                     // -> DIVISÃO
       resultado = divisao(a,b);
     }
     
@@ -78,7 +76,7 @@ public class Calculadora implements Calculavel{
   }
   
   /**
-   * @brief   Soma o valor de "a" com o valor de "b"
+   * Soma o valor de "a" com o valor de "b"
    * @param   a   Primeiro operando 
    * @param   b   Segundo operando
    * @return  +(a,b)
@@ -88,7 +86,7 @@ public class Calculadora implements Calculavel{
   }
   
   /**
-   * @brief   Subtrai o valor de "a" pelo valor de "b"
+   * Subtrai o valor de "a" pelo valor de "b"
    * @param   a   Primeiro operando 
    * @param   b   Segundo operando
    * @return  -(a,b)
@@ -98,7 +96,7 @@ public class Calculadora implements Calculavel{
   }
   
   /**
-   * @brief   Multiplica o valor de "a" pelo valor de "b"
+   * Multiplica o valor de "a" pelo valor de "b"
    * @param   a   Primeiro operando 
    * @param   b   Segundo operando
    * @return  *(a,b)
@@ -108,31 +106,37 @@ public class Calculadora implements Calculavel{
   }
   
   /**
-   * @brief   Dividi o valor de "a" pelo valor de "b"
+   * Dividi o valor de "a" pelo valor de "b"
    * @param   a   Primeiro operando 
    * @param   b   Segundo operando
    * @return  /(a,b)
    */ 
   public double divisao(double a, double b){  
-    if(b == 0){     /// DIVISÃO INVÁLIDA: Divisão por zero
-      System.out.println("Entrada invalida!");
-      return 0;
-    }  
-    else{
-      return a/b;
-    }  
+    
+    try{ 
+   
+       if(b == 0){     // -> DIVISÃO INVÁLIDA: Divisão por zero
+         throw new ArithmeticException("Divisao por zero eh invalida...");
+       }   
+       return a/b;
+    }
+    catch( ArithmeticException e){
+       System.err.println( e.getMessage() );
+       return 0;  
+    }
+   
   }
   
   /**
-   * @brief   Imprime o historico de resultado 
+   * Imprime o historico de resultado 
   */
   public void imprimirHistorico(){
     
     System.out.println("\n\nHISTORICO:");
     
-    /// ENQUANTO A PILHA NÃO ESTÁ VAZIA, IMPRIME
-    while(!historico.empty()){
-      System.out.println(historico.pop());
+    // -> ENQUANTO A PILHA NÃO ESTÁ VAZIA, IMPRIME
+    while( !historico.empty() ){
+      System.out.println( historico.pop() );
     }
     
   }
